8c9be42d731a0f94a3427f315f612dc47349f2d4
Bowen Chan <Bowenmchan@gmail.com>
2015-10-01 12:42:05 -0700

    Add comments and clean up

--- BioInformaticMatrix/src/BioinformaticMatrix.java
+++ BioInformaticMatrix/src/BioinformaticMatrix.java
@@ -7,15 +7,19 @@ public class BioinformaticMatrix {
 	private static final String ARRAY_NUMBER = "4 -1 -2 -2 0 -1 -1 0 -2 -1 -1 -1 -1 -2 -1 1 0 -3 -2 0 -2 -1 0 -4 -1 5 0 -2 -3 1 0 -2 0 -3 -2 2 -1 -3 -2 -1 -1 -3 -2 -3 -1 0 -1 -4 -2 0 6 1 -3 0 0 0 1 -3 -3 0 -2 -3 -2 1 0 -4 -2 -3 3 0 -1 -4 -2 -2 1 6 -3 0 2 -1 -1 -3 -4 -1 -3 -3 -1 0 -1 -4 -3 -3 4 1 -1 -4 0 -3 -3 -3 9 -3 -4 -3 -3 -1 -1 -3 -1 -2 -3 -1 -1 -2 -2 -1 -3 -3 -2 -4 -1 1 0 0 -3 5 2 -2 0 -3 -2 1 0 -3 -1 0 -1 -2 -1 -2 0 3 -1 -4 -1 0 0 2 -4 2 5 -2 0 -3 -3 1 -2 -3 -1 0 -1 -3 -2 -2 1 4 -1 -4 0 -2 0 -1 -3 -2 -2 6 -2 -4 -4 -2 -3 -3 -2 0 -2 -2 -3 -3 -1 -2 -1 -4 -2 0 1 -1 -3 0 0 -2 8 -3 -3 -1 -2 -1 -2 -1 -2 -2 2 -3 0 0 -1 -4 -1 -3 -3 -3 -1 -3 -3 -4 -3 4 2 -3 1 0 -3 -2 -1 -3 -1 3 -3 -3 -1 -4 -1 -2 -3 -4 -1 -2 -3 -4 -3 2 4 -2 2 0 -3 -2 -1 -2 -1 1 -4 -3 -1 -4 -1 2 0 -1 -3 1 1 -2 -1 -3 -2 5 -1 -3 -1 0 -1 -3 -2 -2 0 1 -1 -4 -1 -1 -2 -3 -1 0 -2 -3 -2 1 2 -1 5 0 -2 -1 -1 -1 -1 1 -3 -1 -1 -4 -2 -3 -3 -3 -2 -3 -3 -3 -1 0 0 -3 0 6 -4 -2 -2 1 3 -1 -3 -3 -1 -4 -1 -2 -2 -1 -3 -1 -1 -2 -2 -3 -3 -1 -2 -4 7 -1 -1 -4 -3 -2 -2 -1 -2 -4 1 -1 1 0 -1 0 0 0 -1 -2 -2 0 -1 -2 -1 4 1 -3 -2 -2 0 0 0 -4 0 -1 0 -1 -1 -1 -1 -2 -2 -1 -1 -1 -1 -2 -1 1 5 -2 -2 0 -1 -1 0 -4 -3 -3 -4 -4 -2 -2 -3 -2 -2 -3 -2 -3 -1 1 -4 -3 -2 11 2 -3 -4 -3 -2 -4 -2 -2 -2 -3 -2 -1 -2 -3 2 -1 -1 -2 -1 3 -3 -2 -2 2 7 -1 -3 -2 -1 -4 0 -3 -3 -3 -1 -2 -2 -3 -3 3 1 -2 1 -1 -2 -2 0 -3 -1 4 -3 -2 -1 -4 -2 -1 3 4 -3 0 1 -1 0 -3 -4 0 -3 -3 -2 0 -1 -4 -3 -3 4 1 -1 -4 -1 0 0 1 -3 3 4 -2 0 -3 -3 1 -1 -3 -1 0 -1 -3 -2 -2 1 4 -1 -4 0 -1 -1 -1 -2 -1 -1 -1 -1 -1 -1 -1 -1 -1 -2 0 0 -2 -1 -1 -1 -1 -1 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 1 ";
 	public static Map<String, Integer> dictionary;
 	public static int[][] matrix;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//create the foundational array and map
 		dictionary = createMap(ARRAY_WORD);
 		matrix = createArray(ARRAY_NUMBER);
 			
+		//obtain the user's string sequence
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Enter the String as the base: ");
 		String input = scan.nextLine();
 		
+		//obtain the gene sequence(with an assumption of 4 character)
 		System.out.print("Enter the gene you would like to compare: ");
 		String gene = scan.nextLine();
 		String[] geneW = gene.split("");
@@ -38,11 +42,11 @@ public class BioinformaticMatrix {
 	 */
 	private static int calculateBestScore(String original, String gene, String geneW, int pos)
 	{
+		//intialize local variable
 		int total = 0;
-		int temp = 0;
 		int loc = 0;
 		
-		
+		//set each word to their designated row
 		String[] genex = gene.split("");
 		int row = dictionary.get(genex[0]);
 		int row1 = dictionary.get(genex[1]);
@@ -50,22 +54,28 @@ public class BioinformaticMatrix {
 		int row3 = dictionary.get(genex[3]);
 
 		String[] word = original.split("");
+		//loop through the whole word
 		for(int i = 0; i < word.length - 3; i++)
 		{
+			int temp = 0;
+			//find when the letter is equal to itself in the string to determine its
+			//biggest sum
 			if(word[i].equals(geneW))
 			{
+				//determine the sum of the word
 				temp += matrix[row][dictionary.get(word[i - pos])];
 				temp += matrix[row1][dictionary.get(word[i - pos + 1])];
 				temp += matrix[row2][dictionary.get(word[i - pos + 2])];
 				temp += matrix[row3][dictionary.get(word[i - pos+ 3])];
+				//find the maximum number
 				if(temp > total)
 				{
 					System.out.println("Letter is: "+ word[i - pos ] + word[i -pos +1] + word[i -pos + 2] + word[i -pos +3]);
 					total = temp;
-					temp = 0;
+					
 					loc = i;
 				}
-				temp = 0;
+				
 			}
 		}
 		System.out.println("Current Location: " + loc);
