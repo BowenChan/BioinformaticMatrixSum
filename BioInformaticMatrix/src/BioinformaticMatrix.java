676026b5f81ee0f9acc8e7fcf0c5ee124c84e9dc
Bowen Chan <Bowenmchan@gmail.com>
2015-11-30 10:28:16 -0800

    Psuedo table

--- BioInformaticMatrix/src/BioinformaticMatrix.java
+++ BioInformaticMatrix/src/BioinformaticMatrix.java
@@ -6,28 +6,38 @@ public class BioinformaticMatrix {
 	private static final String ARRAY_WORD = "A R N D C Q E G H I L K M F P S T W Y V B Z X *";
 	private static final String ARRAY_NUMBER = "4 -1 -2 -2 0 -1 -1 0 -2 -1 -1 -1 -1 -2 -1 1 0 -3 -2 0 -2 -1 0 -4 -1 5 0 -2 -3 1 0 -2 0 -3 -2 2 -1 -3 -2 -1 -1 -3 -2 -3 -1 0 -1 -4 -2 0 6 1 -3 0 0 0 1 -3 -3 0 -2 -3 -2 1 0 -4 -2 -3 3 0 -1 -4 -2 -2 1 6 -3 0 2 -1 -1 -3 -4 -1 -3 -3 -1 0 -1 -4 -3 -3 4 1 -1 -4 0 -3 -3 -3 9 -3 -4 -3 -3 -1 -1 -3 -1 -2 -3 -1 -1 -2 -2 -1 -3 -3 -2 -4 -1 1 0 0 -3 5 2 -2 0 -3 -2 1 0 -3 -1 0 -1 -2 -1 -2 0 3 -1 -4 -1 0 0 2 -4 2 5 -2 0 -3 -3 1 -2 -3 -1 0 -1 -3 -2 -2 1 4 -1 -4 0 -2 0 -1 -3 -2 -2 6 -2 -4 -4 -2 -3 -3 -2 0 -2 -2 -3 -3 -1 -2 -1 -4 -2 0 1 -1 -3 0 0 -2 8 -3 -3 -1 -2 -1 -2 -1 -2 -2 2 -3 0 0 -1 -4 -1 -3 -3 -3 -1 -3 -3 -4 -3 4 2 -3 1 0 -3 -2 -1 -3 -1 3 -3 -3 -1 -4 -1 -2 -3 -4 -1 -2 -3 -4 -3 2 4 -2 2 0 -3 -2 -1 -2 -1 1 -4 -3 -1 -4 -1 2 0 -1 -3 1 1 -2 -1 -3 -2 5 -1 -3 -1 0 -1 -3 -2 -2 0 1 -1 -4 -1 -1 -2 -3 -1 0 -2 -3 -2 1 2 -1 5 0 -2 -1 -1 -1 -1 1 -3 -1 -1 -4 -2 -3 -3 -3 -2 -3 -3 -3 -1 0 0 -3 0 6 -4 -2 -2 1 3 -1 -3 -3 -1 -4 -1 -2 -2 -1 -3 -1 -1 -2 -2 -3 -3 -1 -2 -4 7 -1 -1 -4 -3 -2 -2 -1 -2 -4 1 -1 1 0 -1 0 0 0 -1 -2 -2 0 -1 -2 -1 4 1 -3 -2 -2 0 0 0 -4 0 -1 0 -1 -1 -1 -1 -2 -2 -1 -1 -1 -1 -2 -1 1 5 -2 -2 0 -1 -1 0 -4 -3 -3 -4 -4 -2 -2 -3 -2 -2 -3 -2 -3 -1 1 -4 -3 -2 11 2 -3 -4 -3 -2 -4 -2 -2 -2 -3 -2 -1 -2 -3 2 -1 -1 -2 -1 3 -3 -2 -2 2 7 -1 -3 -2 -1 -4 0 -3 -3 -3 -1 -2 -2 -3 -3 3 1 -2 1 -1 -2 -2 0 -3 -1 4 -3 -2 -1 -4 -2 -1 3 4 -3 0 1 -1 0 -3 -4 0 -3 -3 -2 0 -1 -4 -3 -3 4 1 -1 -4 -1 0 0 1 -3 3 4 -2 0 -3 -3 1 -1 -3 -1 0 -1 -3 -2 -2 1 4 -1 -4 0 -1 -1 -1 -2 -1 -1 -1 -1 -1 -1 -1 -1 -1 -2 0 0 -2 -1 -1 -1 -1 -1 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 1 ";
 	public static Map<String, Integer> dictionary;
+	private static String[] sequence1;
+	private static String[] sequence2;
 	public static int[][] blosumMatrix;
-	public static int[][] smithWatersman;
+	public static String[][] smithWatersman;
+	private final int MATCH = 2;
+	private final int MISMATCH = -1;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//create the foundational array and map
 		dictionary = createMap(ARRAY_WORD);
 		blosumMatrix = createArray(ARRAY_NUMBER);
-			
-		//obtain the user's string sequence
 		Scanner scan = new Scanner(System.in);
-		System.out.print("Enter the String as the base: ");
-		String input = scan.nextLine();
+		createSmithWater("Hello", "Hello");
+		//Setting the Gap Penalty
+		System.out.println("");
 		
+		/*
+		//obtain the user's string sequence
+		
+		System.out.print("Enter the String as the base: ");
+		String input = scan.nextLine();	
 		//obtain the gene sequence(with an assumption of 4 character)
 		System.out.print("Enter the gene you would like to compare: ");
 		String gene = scan.nextLine();
 		String[] geneW = gene.split("");
+		
 		System.out.println(calculateBestScore(input,gene,geneW[0],0));
 		System.out.println(calculateBestScore(input,gene,geneW[1],1));
 		System.out.println(calculateBestScore(input,gene,geneW[2],2));
 		System.out.println(calculateBestScore(input,gene,geneW[3],3));
+		*/
 	}
 	
 	/**
@@ -132,16 +142,67 @@ public class BioinformaticMatrix {
 	}
 	
 	private static int[][] createSmithWater(String seq1, String seq2){
-		
+
+		seq1 = "-" + seq1;
+		seq2 = "-" + seq2;
+
+		int row = seq2.length() + 1;
+		int column = seq1.length() + 1;
+		sequence1 = seq1.split("");
+		sequence2 = seq2.split("");
+		System.out.println(sequence1[1]);
+		System.out.println(sequence2[1]);
+		smithWatersman = new String[row][column];
 		//initialize the first row and column
-		for(int row = 0;row < seq2.length();row++)
+		smithWatersman[0][0] = " ";
+		for(int i = 1;i < row;i++)
 		{
-			smithWatersman[row][0] = 0;
+			smithWatersman[i][0] = sequence2[i - 1];
 		}
-		for(int column = 0; column < seq1.length(); column++)
+		for(int j = 1; j < column; j++)
 		{
-			smithWatersman[0][column] = 0;
+			smithWatersman[0][j] = sequence1[j - 1];
+		}
+		for(int i = 1; i < row; i++)
+		{
+			for(int j = 1; j < column; j++)
+			{
+				if(i == 1 || j == 1)
+				{
+					smithWatersman[i][j]= "" + 0;
+				}
+				else
+					smithWatersman[i][j] = "" + 1;
+				
+			}
+		}
+		/*
+		//constant gap scoring
+		for(int i = 1; i < row; i++)
+		{
+			for(int j = 1; j < column; j++){
+				smithWatersman[i][j] = checkScore(i, j);
+			}
+		}*/
+		for(int i = 0; i < row;i++)
+		{
+			for(int j = 0; j < column; j++)
+			{
+				System.out.print(smithWatersman[i][j]+ " ");
+			}
+			System.out.println();
 		}
 		return null;
+	}
+	
+	private static int checkScore(int row, int column){
+		int max = 0;
+	
+		return 0;
+	}
+	
+	private static boolean match(char a, char b)
+	{
+		return a == b;
 	}
 }
