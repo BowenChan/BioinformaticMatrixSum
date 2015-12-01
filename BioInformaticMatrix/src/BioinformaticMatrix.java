ca5f3c82b1b6cf53b1e92223c8ef2d77bfe3274d
Bowen Chan <Bowenmchan@gmail.com>
2015-11-30 20:19:46 -0800

    Finished up the local alignment and working on user Input

--- BioInformaticMatrix/src/BioinformaticMatrix.java
+++ BioInformaticMatrix/src/BioinformaticMatrix.java
@@ -1,8 +1,12 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 import javax.print.DocFlavor.STRING;
+import javax.security.auth.kerberos.KerberosKey;
+
+import org.omg.CORBA.MARSHAL;
 
 public class BioinformaticMatrix {
 	private static final String ARRAY_WORD = "A R N D C Q E G H I L K M F P S T W Y V B Z X *";
@@ -14,8 +18,11 @@ public class BioinformaticMatrix {
 	private static String[] sequence2;
 	public static int[][] blosumMatrix;
 	public static String[][] smithWatersman;
-	private static final int MATCH = 2;
-	private static final int MISMATCH = -1;
+	private static int MATCH = 1;
+	private static int MISMATCH = -1;
+	private static  int GAP = -1;
+	private static int o = -1;
+	private static int l;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -23,11 +30,34 @@ public class BioinformaticMatrix {
 		dictionary = createMap(ARRAY_WORD);
 		blosumMatrix = createArray(ARRAY_NUMBER);
 		Scanner scan = new Scanner(System.in);
-		createSmithWater("ACACACTA", "AGCACACA");
-		//Setting the Gap Penalty
-
 		
+		// Testing out three test cases
+		
+		System.out.println("Test Case with ACACACTA and AGCACACA: Match = 2, MisMatch = -1 Gap Penalty set to -1 constant");
+		MATCH = 2;
+		createSmithWater("ACACACTA", "AGCACACA");
 		printSmithWaterman();
+		determineBestScore();
+		
+		System.out.println();
+		System.out.println();
+		
+		System.out.println("Test Case with ATCAGAGTC and GTCAGTCA: Match = 2, MisMatch = -1 Gap Penalty set to -2 constant");
+		GAP = -2;
+		createSmithWater("ATCAGAGTC", "GTCAGTCA");
+		printSmithWaterman();
+		determineBestScore();
+
+		System.out.println();
+		System.out.println();
+		System.out.println("Test Case with AACAGTCT and AGT: Match = 1, MisMatch = -1 Gap Penalty set to -2 constant");
+		MATCH = 1;
+		createSmithWater("AACAGTCT", "AGT");
+		printSmithWaterman();
+		determineBestScore();
+		
+		
+		
 		/*
 		//obtain the user's string sequence
 		
@@ -149,6 +179,10 @@ public class BioinformaticMatrix {
 	/**
 	 * Method Create Smith Watersman Array
 	 * 
+	 * This method will split the two strings and by using the gap penalty
+	 * and the match/mismatch score, it will determine the best possible
+	 * SmithWatersman matrix for the calculation of the best local alignment
+	 * 
 	 * @param seq1
 	 * @param seq2
 	 * @return
@@ -167,7 +201,7 @@ public class BioinformaticMatrix {
 		//deliminate/split the sequence into an array to easily traverse
 		sequence1 = seq1.split("");
 		sequence2 = seq2.split("");
-		
+	
 		//setting up the array
 		smithWatersman = new String[row][column];
 		
@@ -196,15 +230,6 @@ public class BioinformaticMatrix {
 				
 			}
 		}
-		/*
-		//constant gap scoring
-		for(int i = 1; i < row; i++)
-		{
-			for(int j = 1; j < column; j++){
-				smithWatersman[i][j] = checkScore(i, j);
-			}
-		}*/
-		
 
 		return null;
 	}
@@ -212,26 +237,31 @@ public class BioinformaticMatrix {
 	/**
 	 * Method Check Score
 	 * 
+	 * This method checks for the which number will be inserted for the row and column
+	 * of the given location. It will determine this by taking the maximum of the top,
+	 * diagonal, and left number of the given location
+	 * 
 	 * @param rowLoc
 	 * @param columnLoc
-	 * @return
+	 * @return the max number 
 	 */
 	private static int checkScore(int rowLoc, int columnLoc){
 		int max = 0;
-		int change;
-		//System.out.println(smithWatersman[rowLoc][0].equals(smithWatersman[0][columnLoc]));
+		int change; // determine the match or mismatch number
 
 		if(match(rowLoc, columnLoc)){
-
+			l = 0;
 			change = MATCH;
 		}
 		else {
+			l++;
 			change = MISMATCH;
 		}
 		
-		max = Math.max(max, Math.max(Integer.parseInt(smithWatersman[rowLoc - 1][columnLoc]) -1,
+		//determine the max number
+		max = Math.max(max, Math.max(Integer.parseInt(smithWatersman[rowLoc - 1][columnLoc]) + GAP,
 						Math.max(Integer.parseInt(smithWatersman[rowLoc-1][columnLoc-1])+ change, 
-								Integer.parseInt(smithWatersman[rowLoc][columnLoc-1])-1)));
+								Integer.parseInt(smithWatersman[rowLoc][columnLoc-1]) + GAP)));
 		
 		return max;
 	}
@@ -239,9 +269,10 @@ public class BioinformaticMatrix {
 	/**
 	 * Method Check Match
 	 * 
+	 * This method returns a boolean if the two characters are matching or not
 	 * @param rowLoc
 	 * @param columnLoc
-	 * @return
+	 * @return a boolean 
 	 */
 	private static boolean match(int rowLoc, int columnLoc)
 	{
@@ -267,8 +298,146 @@ public class BioinformaticMatrix {
 		}
 	}
 
-	
+	/**
+	 * Method determining the best score
+	 * 
+	 * This method prints out and determines the best local alignment for the two
+	 * given strands
+	 */
 	private static void determineBestScore(){
+		//creates the two variable for the final result
+		ArrayList<String> sequenceRes = new ArrayList<String>();
+		ArrayList<String> sequenceRes2 = new ArrayList<String>();
 		
+		//this variable will be implemeneted later to determine the path of all possible
+		//local alignment
+		ArrayList<Pair> path = new ArrayList<Pair>();
+		
+		//this helps the iteration and traveling frm the end of the matrix to the beginning
+		int i = row - 1;
+		int j = column - 1;
+		
+		//iterating through the whole matrix
+		while(i != 1 || j != 1){
+			//checks the base case if the indexes are at the end and can either
+			//only traverse left or up
+			if((i - 1) == 0 && j > 0){
+				sequenceRes.add(smithWatersman[0][j]);
+				sequenceRes2.add("-");
+				j--;
+			}
+			else if ((j - 1 ) == 0 && i > 0)
+			{
+				sequenceRes.add("-");
+				sequenceRes2.add(smithWatersman[i][0]);
+				i--;
+			}
+			
+			//checks for the numbers to traverse left, diag, or up
+			else{
+				
+				//store all the 3 integers that are being checked
+				int diag = Integer.parseInt(smithWatersman[i-1][j-1]);
+				int left = Integer.parseInt(smithWatersman[i][j-1]);
+				int up = Integer.parseInt(smithWatersman[i-1][j]);
+
+				//if the diagonal is greater then the left number
+				if(diag > left){
+					//checks if the diag is the greatest number
+					if(diag > up){
+						sequenceRes.add(smithWatersman[0][j]);
+						sequenceRes2.add(smithWatersman[i][0]);
+						i--;
+						j--;
+					}
+					//if the diag is not greater then the number above then it will traverse up
+					else{
+						sequenceRes.add("-");
+						sequenceRes2.add(smithWatersman[i][0]);
+						i--;
+					}
+				}
+				//checks if the left is greater than the diag number
+				else if(left > diag){
+					//if the left number is greater than the diag number then check
+					//if it is greater then the number above
+					if(left > up){
+						sequenceRes.add(smithWatersman[0][j]);
+						sequenceRes2.add("-");
+						j--;
+					}
+					else{
+						sequenceRes.add("-");
+						sequenceRes2.add(smithWatersman[i][0]);
+						i--;
+					}
+				}
+				//checks if the number above is greater then then number at the diagonal
+				else if(up > diag){
+					//checks if the number above is greater then the number to the left
+					if(up > left)
+					{
+						sequenceRes.add("-");
+						sequenceRes2.add(smithWatersman[i][0]);
+						i--;
+						
+					}
+					else{
+						sequenceRes.add(smithWatersman[0][j]);
+						sequenceRes2.add("-");
+						j--;
+					}
+					
+				}
+				
+				//deals the case that if the number to the left or above is not greater than
+				//but equals to the number diagonal then you are required to move
+				//diagonal.
+				else if (diag == left || diag == up)
+				{
+					sequenceRes.add(smithWatersman[0][j]);
+					sequenceRes2.add(smithWatersman[i][0]);
+					i--;
+					j--;
+				}
+
+			}
+
+		}
+		
+		//prints out the solution
+		for(int first = sequenceRes.size()-1; first >= 0;first--)
+		{
+			System.out.print(sequenceRes.get(first));
+		}
+		System.out.println();
+		for(int second = sequenceRes2.size() -1 ; second >= 0;second--)
+		{
+			System.out.print(sequenceRes2.get(second));
+		}
 	}
+	
+	/**
+	 * Method bestScoreHelper
+	 * 
+	 * TO BE IMPLEMENTED
+	 * 
+	 * a method that will help recursively determine all the possible alignment
+	 * 
+	 * @param i
+	 * @param j
+	 * @param path
+	 * @param total
+	 * @return
+	 */
+	private static ArrayList<Pair> bestScoreHelper(int i, int j,ArrayList<Pair> path, int total)
+	{
+		if(i == 0 || j == 0)
+		{
+			return path;
+		}
+		return bestScoreHelper(i,j,path, total);
+	}
+	
+	
 }
